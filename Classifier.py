875a450a4f1fdcf63a5b5e35a1ae18ac04cb7023
Shingirai98 <https://github.com/Shingirai98>
2021-06-16 02:00:57 +0200

    [ADDED] Linear Forward Passing functionality

--- Classifier.py
+++ Classifier.py
@@ -1,139 +1,116 @@
-import copy
-import os
-import random
-import numpy as np
 import torch
 import torchvision
-import matplotlib.pyplot as plt
 from torch.utils.data import random_split
-from torchvision import datasets, transforms
+from torchvision import transforms
 from torch import nn, optim
 import torch.nn.functional as F
-import pandas as pd
 from torchvision.datasets import ImageFolder
-from sklearn.metrics import confusion_matrix
-import requests ##
 from PIL import Image
-from io import BytesIO
 
-import copy
+
 
 batch = 64
-T = torchvision.transforms.Compose([torchvision.transforms.Grayscale(num_output_channels=1), torchvision.transforms.ToTensor()])
+T = torchvision.transforms.Compose(
+    [torchvision.transforms.Grayscale(num_output_channels=1), torchvision.transforms.ToTensor()])
 root = "./MNIST_JPGS/trainingSet/trainingSet"
 data = ImageFolder(root, transform=T)
+img = Image.open("MNIST_JPGS/trainingSet/trainingSet/7/img_6.jpg",)
+img_tensor = transforms.ToTensor()(img).unsqueeze_(0)
 
-train_size = int(0.1*len(data))
-val_size = len(data)-train_size
-
+train_size = int(0.8*len(data))
+val_size = len(data) - train_size
 train_data, val_data = random_split(data, [train_size, val_size])
 
-#val_data = ImageFolder('./MNSIT/trainingSet/trainingSet/', transform=T)
+# val_data = ImageFolder('./MNSIT/trainingSet/trainingSet/', transform=T)
 
-train_data_loader = torch.utils.data.DataLoader(train_data, batch_size = batch)
-val_data_loader = torch.utils.data.DataLoader(val_data, batch_size = batch)
+train_data_loader = torch.utils.data.DataLoader(train_data, batch_size=batch, shuffle=True)
+val_data_loader = torch.utils.data.DataLoader(val_data, batch_size=batch, shuffle=True)
 
-#plt.imshow(train_data[0][0][0], cmap='gray')
+# for d in train_data_loader:
+#     print(d)
+#
+#     break
 
+#x, y = d[0][0], data[1][0]
+#print(y.shape)
 
-def create_lenet():
-    model = nn.Sequential(
-        nn.Conv2d(1, 6, 5, padding=2),
-        nn.ReLU(),
-        nn.AvgPool2d(2, stride=2),
-        nn.Conv2d(6, 16, 5, padding=0),
-        nn.ReLU(),
-        nn.AvgPool2d(2, stride=2),
-        nn.Flatten(),
-        nn.Linear(400, 120),
-        nn.ReLU(),
-        nn.Linear(120, 84),
-        nn.ReLU(),
-        nn.Linear(84, 10)
-    )
-    return model
+# plt.imshow(d[0][0].view(28, 28))
+# plt.show()
 
+total = 0
+counter_dict = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0}
 
-def validate_model(model, data):
-    total = 0
-    correct = 0
-    for i, (images, labels) in enumerate(data):
-        images = images.to("cpu") # might skip this
-        x = model(images)
-        value, pred = torch.max(x, 1)
-        pred = pred.data.cpu()
-        total += x.size(0)
-        correct += torch.sum(pred == labels)
+for da in train_data_loader:
+    Xs, ys = da
+    for y in ys:
+        counter_dict[int(y)] += 1
+        total += 1
 
-    return correct/total
+#print(counter_dict)
+
+# for i in counter_dict:
+#     print(f"{i}: {counter_dict[i] / total * 100}")
 
 
-def train(epochs = 3, learning_rate = 1e-3, device="cpu"):
-    cnn = create_lenet().to(device)
-    accuracies = []
-    cec = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(cnn.parameters(), lr=1e-3)
-    max_accuracy = 0
+class Network(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # define Layers
+        self.fc1 = nn.Linear((28 * 28), 64)  # input and output Linear:fully connected
+        self.fc2 = nn.Linear(64, 64)
+        self.fc3 = nn.Linear(64, 64)
+        self.fc4 = nn.Linear(64, 10)
 
-    for epoch in range(epochs):
-        for i, (images, labels) in enumerate (train_data_loader):
-            images = images.to(device)
-            labels = labels.to(device)
-            optimizer.zero_grad()
-            pred = cnn(images)
-            loss = cec(pred, labels)
-            loss.backward()
-            optimizer.step()
-        accuracy = float(validate_model(cnn, val_data_loader))
-        accuracies.append(accuracy)
-        if accuracy > max_accuracy:
-            best_model = copy.deepcopy(cnn)
-            max_accuracy = accuracy
-            print("Saving best model with Accuracy: ", accuracy)
-        print("Epoch:", epoch+1, "Accuracy: ",accuracy)
-    plt.plot(accuracies)
-    return best_model
-
-lenet = train(5, "cpu")
+    def forward(self, x):  # feed-forward net
+        x = F.relu(self.fc1(x))  # Rectified Linear Activation Function
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
+        # multiclass variance
+        return F.log_softmax(x, dim=1)
 
 
-torch.save(lenet.state_dict(), "model.pth")
-def predict_dl(model, data):
-    y_pred = []
-    y_true = []
-    for i, (images, labels) in enumerate(data):
-        images = images.cpu()
-        x = model(images)
-        value, pred = torch.max(x, 1)
-        pred = pred.data.cpu()
-        y_pred.extend(list(pred.numpy()))
-        y_true.extend(list(labels.numpy()))
-    return np.array(y_pred), np.array(y_true)
+net = Network()
+#print(net)
 
+X = torch.rand((28, 28))
+X = X.view(-1, 28 * 28)  # -1 is to show that you are not aware of the shape (any size data input)
+out = net(X)
+#print(out)
 
-y_pred, y_true = predict_dl(lenet, val_data_loader)
-pd.DataFrame(confusion_matrix(y_true, y_pred, labels=np.arange(0,10)))
+# Transfer Learning
+optimizer = optim.Adam(net.parameters(), lr=0.001)
 
-def inference(path, model, device):
-    r = requests.get(path)
-    with BytesIO(r.content) as f:
-        img = Image.open(f).convert(mode="L")
-        img = img.resize((28, 28))
-        x = (255 - np.expand_dims(np.array(img), -1))/255.
-    with torch.no_grad():
-        pred = model(torch.unsqueeze(T(x), axis=0).float().to(device))
-        return F.softmax(pred, dim=-1).cpu().numpy()
+EPOCHS = 5
+for epoch in range(EPOCHS):
+    for single_data in train_data_loader:
+        X, y = single_data
+        net.zero_grad()
+        output = net(X.view(-1, 28*28))
+        # loss for single data value is calculated with nll_loss
+        loss = F.nll_loss(output, y)
+        loss.backward()
+        #adjust weights
+        optimizer.step()
 
-path = "https://previews.123rf.com/images/aroas/aroas1704/aroas170400068/79321959-handwritten-sketch-black-number-8-on-white-background.jpg"
-r = requests.get(path)
-with BytesIO(r.content) as f:
-    img = Image.open(f).convert(mode="L")
-    img = img.resize((28, 28))
-x = (255 - np.expand_dims(np.array(img), -1))/255.
+    #print(loss)
 
-plt.imshow(x.squeeze(-1), cmap="gray")
+correct = 0
+total = 0
 
-pred = inference(path, lenet, device="cpu")
-pred_idx = np.argmax(pred)
-print(f"Predicted: {pred_idx}, Prob: {pred[0][pred_idx]*100} %")
-print(pred)
+with torch.no_grad():
+    for d in val_data_loader:
+        X, y = d
+        #print(X[0])
+        #print(y[0])
+
+        output = net(X.view(-1, 28*28))
+        #print(output)
+        for idx, i in enumerate(output):
+            if torch.argmax(i) == y[idx]: # this is the output guessed by Neural Network
+                correct += 1
+            total += 1
+
+print(torch.argmax(net((img_tensor).view(-1, 784))))
+print("Accuracy: ", round(correct/total, 3))
+
